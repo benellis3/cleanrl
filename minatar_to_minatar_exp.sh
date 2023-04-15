96d8b7d90b1a250a7ca0d576a9673874ad4d8a29
benellis3 <benellis@robots.ox.ac.uk>
2023-04-15 16:35:10 +0100

    Fix run script

--- minatar_to_minatar_exp.sh
+++ minatar_to_minatar_exp.sh
@@ -32,36 +32,36 @@ wait
 ./run.sh 2 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 &
 ./run.sh 3 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 &
 
-./run.sh 4 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 0 --transfer-only &
+./run.sh 4 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 0 --transfer-only  True &
 
-./run.sh 5 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 1 --transfer-only &
-./run.sh 6 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 2 --transfer-only &
-./run.sh 7 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 --transfer-only &
+./run.sh 5 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 1 --transfer-only  True &
+./run.sh 6 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 2 --transfer-only  True &
+./run.sh 7 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 --transfer-only  True &
 wait
-./run.sh 0 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 --transfer-only &
+./run.sh 0 Breakout-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 --transfer-only  True &
 
 
-./run.sh 1 Asterix-v5 --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 0 --transfer-only &
+./run.sh 1 Asterix-v5 --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 0 --transfer-only  True &
 
 
-./run.sh 2 Asterix-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 1 --transfer-only &
+./run.sh 2 Asterix-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 1 --transfer-only  True &
 
-./run.sh 3 Asterix-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 2 --transfer-only &
+./run.sh 3 Asterix-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 2 --transfer-only  True &
 
 
-./run.sh 4 Asterix-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 --transfer-only &
-./run.sh 5 Asterix-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 --transfer-only &
+./run.sh 4 Asterix-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 --transfer-only  True &
+./run.sh 5 Asterix-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 --transfer-only  True &
 
-./run.sh 6 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 0 --transfer-only &
-./run.sh 7 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 1 --transfer-only &
+./run.sh 6 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 0 --transfer-only True &
+./run.sh 7 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 1 --transfer-only True &
 wait
-./run.sh 0 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 2 --transfer-only &
-./run.sh 1 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 --transfer-only &
-./run.sh 2 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 --transfer-only &
+./run.sh 0 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 2 --transfer-only True &
+./run.sh 1 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 --transfer-only True &
+./run.sh 2 SpaceInvaders-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 --transfer-only True &
 
-./run.sh 3 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 0 --transfer-only &
+./run.sh 3 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 0 --transfer-only True &
 
-./run.sh 4 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 1 --transfer-only &
-./run.sh 5 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 2 --transfer-only &
-./run.sh 6 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 --transfer-only &
-./run.sh 7 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 --transfer-only &
+./run.sh 4 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 1 --transfer-only True &
+./run.sh 5 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 2 --transfer-only True &
+./run.sh 6 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 3 --transfer-only True &
+./run.sh 7 Freeway-v5   --transfer-environment minatar --total-minatar-steps 10000000 --transfer-num-envs 128 --seed 4 --transfer-only True &
