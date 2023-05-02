c93f9cc53e93a51a6d102959130ae48ac23a7ed9
benellis3 <benellis@robots.ox.ac.uk>
2023-05-02 13:15:27 +0000

    fix run_bc

--- run_bc.sh
+++ run_bc.sh
@@ -1,3 +1,3 @@
 WANDB_API_KEY=$(cat $HOME/.oxwhirl_wandb_api_key)
 
-NV_GPU=$1 nvidia-docker run -e XLA_PYTHON_CLIENT_PREALLOCATE=false -e WANDB_API_KEY=$WANDB_API_KEY -v $HOME/src2/cleanrl:/cleanrl cleanrl:benlis_atari_jax poetry run python -m cleanrl.cleanrl.behaviour_cloning_minatar --env-id $2 --track --wandb-entity benellis3 --wandb-project minatar-minatar-bc ${@:3}
+NV_GPU=$1 nvidia-docker run -e XLA_PYTHON_CLIENT_PREALLOCATE=false -e WANDB_API_KEY=$WANDB_API_KEY -v $HOME/src/cleanrl:/cleanrl cleanrl:benlis_atari_jax poetry run python -m cleanrl.cleanrl.behaviour_cloning_minatar --env-id $2 --track --wandb-entity benellis3 --wandb-project minatar-minatar-bc ${@:3}
