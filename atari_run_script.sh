666755d3d440fdb5c4073b6190ad5e3ea82fb8b5
benellis3 <benellis@robots.ox.ac.uk>
2023-05-11 10:44:49 +0100

    Add atari run script

---
+++ atari_run_script.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+debug=
+# debug=echo
+trap 'onCtrlC' INT
+
+function onCtrlC () {
+  echo 'Ctrl+C is captured'
+  for pid in $(jobs -p); do
+    kill -9 $pid
+  done
+  
+  kill -HUP $( ps -A -ostat,ppid | grep -e '^[Zz]' | awk '{print $2}')
+  exit 1
+}
+
+env_ids=${1:-Asterix-v5,Breakout-v5,SpaceInvaders-v5,Freeway-v5}
+lrs=${2:-0.0002}
+args=${3:-}
+gpus=${3:-0,1,2,3,4,5,6,7}
+threads=${4:-16}
+times=${5:-5}
+
+lrs=(${lrs//,/ })
+gpus=(${gpus//,/ })
+env_ids=(${env_ids//,/ })
+
+echo "ENVS:" ${env_ids[@]}
+echo "THREADS:" $threads
+echo "GPU LIST:" ${gpus[@]}
+echo "TIMES:" $times
+echo "LRs:" ${lrs[@]}
+
+
+
+# run parallel
+count=0
+for lr in "${lrs[@]}"; do
+    for env in "${env_ids[@]}"; do
+        for((i=0;i<times;i++)); do
+            gpu=${gpus[$(($count % ${#gpus[@]}))]}  
+            group="${config}-${tag}"
+            $debug ./run.sh $gpu $env   --transfer-environment atari \
+            --total-minatar-steps 100000000 --transfer-num-envs 8 \
+            --seed $i --transfer-learning-rate $lr &
+            count=$(($count + 1))     
+            if [ $(($count % $threads)) -eq 0 ]; then
+                wait
+            fi
+            # for random seeds
+            sleep $((RANDOM % 3 + 3))
+
+            gpu=${gpus[$(($count % ${#gpus[@]}))]}  
+            $debug ./run.sh $gpu $env   --transfer-environment atari \
+            --total-minatar-steps 100000000 --transfer-num-envs 8 \
+            --seed $i --transfer-learning-rate $lr --freeze-final-layers-on-transfer True &
+            count=$(($count + 1))     
+            if [ $(($count % $threads)) -eq 0 ]; then
+                wait
+            fi
+            # for random seeds
+            sleep $((RANDOM % 3 + 3))
+
+
+            gpu=${gpus[$(($count % ${#gpus[@]}))]}  
+            $debug ./run.sh $gpu $env   --transfer-environment atari \
+            --total-minatar-steps 100000000 --transfer-num-envs 8 \
+            --seed $i --transfer-learning-rate $lr --freeze-final-layers-on-transfer True \
+            --reinitialise-encoder True &
+            count=$(($count + 1))     
+            if [ $(($count % $threads)) -eq 0 ]; then
+                wait
+            fi
+            # for random seeds
+            sleep $((RANDOM % 3 + 3))
+
+            gpu=${gpus[$(($count % ${#gpus[@]}))]}  
+            $debug ./run.sh $gpu $env   --transfer-environment atari \
+            --total-minatar-steps 100000000 --transfer-num-envs 8 \
+            --seed $i --transfer-learning-rate $lr --reinitialise-encoder True &
+            count=$(($count + 1))     
+            if [ $(($count % $threads)) -eq 0 ]; then
+                wait
+            fi
+            # for random seeds
+            sleep $((RANDOM % 3 + 3))
+
+            gpu=${gpus[$(($count % ${#gpus[@]}))]}  
+            $debug ./run.sh $gpu $env   --transfer-environment atari \
+            --total-minatar-steps 100000000 --transfer-num-envs 8 \
+            --seed $i --transfer-learning-rate $lr --transfer-only True &
+            count=$(($count + 1))     
+            if [ $(($count % $threads)) -eq 0 ]; then
+                wait
+            fi
+            # for random seeds
+            sleep $((RANDOM % 3 + 3))
+        done
+    done
+done
+wait
